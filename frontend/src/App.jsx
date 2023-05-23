1358eee4b23a0bc3d467fd8321aa3cc44cd9aa5a
Charuhas Reddy <charuhasreddybalam@gmail.com>
2023-05-23 12:12:01 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import Button from "./components/Button";
-// import { useState } from "react";
-// import * as XLSX from "xlsx";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./assets/styles/App.css";
 
 function App() {
   return (
     <div>
       <h1>Data Insights Hub</h1>
-      <Button />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Button />} />
+          <Route path="/form" element={<h1>Form</h1>} />
+          <Route path="/excel" element={<h1>Excel</h1>} />
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,35 +1,40 @@
 import React from "react";
 import form from "../assets/images/form.svg";
 import excel from "../assets/images/excel.svg";
-// import form from "assets/images/form.svg";
-// import './assets/styles/Button.css'
+import { useNavigate } from "react-router-dom";
 
 function Button() {
-
-  const excelClick = () => {
-    console.log("Excel button clicked");
-
-  }
-
+  let navigate = useNavigate();
   return (
-    <div>
-      <button>
-        <img src={form} className="form-img" alt="form" height={200} width={200} />
+    <div className="btn-group">
+      <button
+        onClick={() => {
+          navigate("/form");
+        }}
+      >
+        <img
+          src={form}
+          className="form-img"
+          alt="form"
+          height="250"
+          width="250"
+        />
       </button>
-      <button onClick={excelClick}>
-        <img src={excel} className="excel-img" alt="excel-image" height={200} width={200} />
+      <button
+        onClick={() => {
+          navigate("/excel");
+        }}
+      >
+        <img
+          src={excel}
+          className="excel-img"
+          alt="excel-image"
+          height="250"
+          width="250"
+        />
       </button>
     </div>
   );
 }
 
 export default Button;
-
-{
-  /* <img
-          src="./assets/images/form.svg"
-          alt="Form"
-          width="50px"
-          height="60px"
-        /> */
-}
