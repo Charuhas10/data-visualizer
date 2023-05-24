af1af07366c8d48c1669825a9baa5ba302335c44
Charuhas Reddy <charuhasreddybalam@gmail.com>
2023-05-24 14:29:52 +0530

    backend changes

--- Backend/app.py
+++ Backend/app.py
@@ -1,39 +1,70 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
+import pandas as pd
 
 app = Flask(__name__)
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client["dataAnalyticsDB"]
 collection = db["dataEntries"]
+output_collection = db["analyticsOutput"]
 
 print(client)
 
 
-@app.route("/api/ingest/form", methods=["POST"])
+@app.route("/api/form", methods=["POST"])
 def ingest_form_data():
     data = request.get_json()  # Retrieve form data
     collection.insert_one(data)  # Store data in the database
     return jsonify({"message": "Data ingested successfully"})
 
 
-@app.route("/api/ingest/excel", methods=["POST"])
+@app.route("/api/excel", methods=["POST"])
 def ingest_excel_data():
     file = request.files["file"]  # Retrieve uploaded Excel sheet
+
     # Process the Excel sheet and extract data
+    df = pd.read_excel(file)
+    extracted_data = df.to_dict(orient="records")
+
     # Store the extracted data in the database
+    collection.insert_many(extracted_data)
+
     return jsonify({"message": "Data ingested successfully"})
 
 
 @app.route("/api/analytics", methods=["GET"])
 def run_analytics():
     # Retrieve data from the database
-    # Perform analytics using a Python script
+    data = list(collection.find({}, {"_id": 0}))
+
+    # Perform analytics
+    analytics_output = {}
+    total_records = len(data)
+
+    # Calculate average rating for the restaurant
+    total_rating = sum(entry["rating"] for entry in data)
+    average_rating = total_rating / total_records
+    analytics_output["average_rating"] = average_rating
+
+    # Analyze the distribution of ratings for different aspects
+    aspects = ["food_quality", "service", "ambience"]
+    ratings_by_aspect = {}
+    for aspect in aspects:
+        ratings = [entry[aspect] for entry in data]
+        ratings_by_aspect[aspect] = {
+            "min_rating": min(ratings),
+            "max_rating": max(ratings),
+            "avg_rating": sum(ratings) / total_records,
+        }
+    analytics_output["ratings_by_aspect"] = ratings_by_aspect
+
     # Store the output in the database
+    output_collection.insert_one(analytics_output)
+
     return jsonify({"message": "Analytics executed successfully"})
 
-
-@app.route("/api/data", methods=["GET"])
+@app.route("/api/getData", methods=["GET"])
 def get_data():
     data = list(collection.find({}, {"_id": 0}))  # Retrieve all data from the database
     return jsonify(data)
